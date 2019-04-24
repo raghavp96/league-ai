498543cbbb8243445e89f1d79377aa15a3017b2d
Raghavprasanna Rajagopalan <raghavp96@gmail.com>
2019-04-23 22:16:51 -0400

    Added functionality to add a specific model for a specified number of epochs (allows us to train, and run the best model without traiining everything)

--- src/app/app.py
+++ src/app/app.py
@@ -41,15 +41,45 @@ def __run(method, onlyUseBasicFeatures):
     rs_write.writeDict(results, epochs=epochs)
 
 
+def __runSpecific(modelName="", method="fixed", onlyUseBasicFeatures=False, epochs=1):
+    # Get training data/labels, test data/labels, development data/labels from data as numpy arrays
+    ((train_data, train_labels), (test_data, test_labels), (dev_data, dev_labels)) = data.get_data(preparation_method=method, onlyUseBasicFeatures=onlyUseBasicFeatures)
+
+    # Get the models
+    model_metadata = models.get_models()
+
+    the_model = None
+    for model in model_metadata:
+        if model["Name"] == modelName:
+           the_model = model
+           break
+    
+    if the_model is not None:
+        the_model["Model"].fit(train_data, train_labels, epochs=epochs)
+
+    # Run on test set
+    test_loss, test_acc = the_model["Model"].evaluate(test_data, test_labels) 
+
+
 if __name__ == "__main__":
     method_mentioned = "fixed"
     onlyUseBasicFeatures = False
+    modelName = ""
+    epochs = 1
     for arg in sys.argv[1:]:
         if arg in ["fixed", "random"]:
             method_mentioned = arg
         elif arg in "onlyBasic":
             onlyUseBasicFeatures = True
-
-    print("Run mode: " + method_mentioned + " with onlyBasicFeatures: " + str(onlyUseBasicFeatures))
-    __run(method_mentioned, onlyUseBasicFeatures)
+        else:
+            try :
+                epochs = int(arg)
+            except ValueError:
+                modelName = arg
+    
+    if modelName != "":
+        __runSpecific(modelName=modelName, method=method_mentioned, onlyUseBasicFeatures=onlyUseBasicFeatures, epochs=epochs)
+    else:
+        print("Run mode: " + method_mentioned + " with onlyBasicFeatures: " + str(onlyUseBasicFeatures))
+        __run(method_mentioned, onlyUseBasicFeatures)
         
